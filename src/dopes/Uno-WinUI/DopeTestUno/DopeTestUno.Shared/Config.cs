50d3f6b8246c83ed3819ea2a0e79aeac6cd5d77b
Jerome Laban <jerome.laban@nventive.com>
2022-05-05 17:07:30 -0400

    feat: Include dopes tests

---
+++ src/dopes/Uno-WinUI/DopeTestUno/DopeTestUno.Shared/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace DopeTestUno
+{
+    public class Config
+    {
+        internal const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=dopetestresults;AccountKey=lI8zyTeF8gAHudSkxkN1Dc+TqFUQR5t2ZGmVMcxnieqWHXnLscrcYL1JW2W9Cpx/0UqZBUMRUZYq9r/MrJDEFA==;EndpointSuffix=core.windows.net";
+        internal const string StorageUrl = "https://dopetestresults.blob.core.windows.net/";
+        internal const string StorageSasToken = "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-23T21:29:14Z&st=2022-02-23T14:29:14Z&spr=https&sig=GXrZZ66s%2F6ferwAO1F8jlVwweQ8Y%2FvdHwtFoegHdrI8%3D";
+    }
+}
---
+++ src/dopes/Uno-dotnet6/DopeTestUno/DopeTestUno.Mobile/Android/MainActivity.Android.cs
@@ -0,0 +1,24 @@
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+
+namespace DopeTestUno
+{
+	[Activity(
+			MainLauncher = true,
+			ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+	public class MainActivity : Windows.UI.Xaml.ApplicationActivity
+	{
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+			Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+			Xamarin.Essentials.Platform.Init(this, bundle);
+        }
+    }
+}
+
